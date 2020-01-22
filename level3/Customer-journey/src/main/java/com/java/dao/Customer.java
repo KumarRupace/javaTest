7c4b9b341dfdabed21e03295cc2fa96663aed2c1
xzhang201 <60187572+xzhang201@users.noreply.github.com>
2020-01-23 00:13:14 +0800

    Update Customer.java

--- level3/Customer-journey/src/main/java/com/java/dao/Customer.java
+++ level3/Customer-journey/src/main/java/com/java/dao/Customer.java
@@ -1,40 +1,70 @@
 package com.java.dao;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+//Author: Zhang Xin
 
 @Entity
 public class Customer {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String firstName;
 	private String lastName;
-
-	protected Customer() {
-	}
-
-	public Customer(String firstName, String lastName) {
+	private long number;
+	@Column(unique = true)
+	private String nric;
+	
+	
+	public Customer(){}
+	public Customer(String nric, String firstName, String lastName, long number){
+		
+		this.nric = nric;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.number = number;
 	}
 
-	@Override
-	public String toString() {
-		return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+	//Getter and setter methods
+	public String getNric() {
+		return nric;
 	}
 
-	public Long getId() {
-		return id;
+	public void setNric(String value) {
+		nric = value;
 	}
-
-	public String getFirstName() {
+	
+	public String getFirstName(){
 		return firstName;
 	}
-
-	public String getLastName() {
+	
+	public void setFirstName(String value){
+		firstName = value;
+	}
+	
+	public String getLastName(){
 		return lastName;
 	}
+	
+	public void setLastName(String value){
+		lastName = value;
+	}
+	
+	public long getNumber(){
+		return number;
+	}
+	
+	public void setNumber(long value){
+		number = value;
+	}
+	
+	//Organise and print out customer's information
+	@Override
+	public String toString() {
+    return String.format(
+        "Customer[id=%d, nric='%s', firstName='%s', lastName='%s', number=%d]",
+        id, nric, firstName, lastName, number);
+  }
+
 }
