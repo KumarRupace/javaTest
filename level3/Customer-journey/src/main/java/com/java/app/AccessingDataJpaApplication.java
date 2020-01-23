61656d3647ab3429c63817b5065fead40cc16e61
richmondchng <richmondchng@gmail.com>
2020-01-24 00:48:04 +0800

    add configuration to scan for spring components

--- level3/Customer-journey/src/main/java/com/java/app/AccessingDataJpaApplication.java
+++ level3/Customer-journey/src/main/java/com/java/app/AccessingDataJpaApplication.java
@@ -1,9 +1,19 @@
 package com.java.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+
 @SpringBootApplication
+@EntityScan(basePackages = { "com.java.dao" })
+@EnableJpaRepositories(basePackages = {"com.java.dao" },
+includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {CrudRepository.class }))
+@ComponentScan(basePackages = { "com.java.dao", "com.java.controller" })
 public class AccessingDataJpaApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataJpaApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication.class, args);
+    }
 }
