0f70a289fb56f0ab1c737886507ea58ad8e3be20
richmondchng <richmondchng@gmail.com>
2020-01-24 17:18:41 +0800

    update entity, add start up script to insert sample data

--- level3/Customer-journey/src/main/java/com/java/dao/Customer.java
+++ level3/Customer-journey/src/main/java/com/java/dao/Customer.java
@@ -1,40 +1,48 @@
 package com.java.dao;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Entity
 public class Customer {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String firstName;
-	private String lastName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("firstName")
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @JsonProperty("lastName")
+    @Column(name = "LAST_NAME")
+    private String lastName;
 
-	protected Customer() {
-	}
+    protected Customer() {
+    }
 
-	public Customer(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+    public Customer(final String firstName, final String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
-	}
+    @Override
+    public String toString() {
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 }
---
+++ level3/Customer-journey/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer(FIRST_NAME, LAST_NAME)
+VALUES
+('John', 'Reeves'),
+('Lisa', 'Hope')
+; 
