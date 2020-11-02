95d3f82992c71dc5a4fb524618460a7de7fdf869
Chang Jun Wei <changjunwei14@gmail.com>
2020-11-02 21:40:31 +0800

    basic configuration setup

--- level3/Customer-journey/src/main/java/com/java/app/AccessingDataJpaApplication.java
+++ level3/Customer-journey/src/main/java/com/java/app/AccessingDataJpaApplication.java
@@ -1,7 +1,16 @@
 package com.java.app;
+import com.java.controller.CustomerController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@EnableJpaRepositories("com...dao")
+@SpringBootApplication(
+	scanBasePackages={"com...app", "com...dao"},
+	scanBasePackageClasses = {CustomerController.class}
+)
 public class AccessingDataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataJpaApplication.class, args);
--- level3/Customer-journey/src/main/java/com/java/dao/Customer.java
+++ level3/Customer-journey/src/main/java/com/java/dao/Customer.java
@@ -1,20 +1,22 @@
 package com.java.dao;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "customer")
 public class Customer {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Column(name = "first_name")
 	private String firstName;
+
+	@Column(name = "last_name")
 	private String lastName;
 
-	protected Customer() {
-	}
+	protected Customer() {}
 
 	public Customer(String firstName, String lastName) {
 		this.firstName = firstName;
@@ -36,5 +38,17 @@ public class Customer {
 
 	public String getLastName() {
 		return lastName;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 }
