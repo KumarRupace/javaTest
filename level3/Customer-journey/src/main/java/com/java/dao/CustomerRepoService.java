980185432d1dd35d7f5a92e5d84b6b74d76838d4
a0114273 <a0114273@u.nus.edu>
2020-11-22 08:52:38 +0800

    Added transactional annotation for spring to manage the db transactions

--- level3/Customer-journey/src/main/java/com/java/dao/CustomerRepoService.java
+++ level3/Customer-journey/src/main/java/com/java/dao/CustomerRepoService.java
@@ -9,7 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class CustomerRepoService {
 
     private final Logger logger = LoggerFactory.getLogger(CustomerRepoService.class);
